e415cc27c0dcaca80b3b092a0ae9b2eae9329539
Prateek Srivastava <workspace.infoga@gmail.com>
2026-01-31 10:32:07 +0530

    fixed deploy

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,7 +34,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:5174",
-        "https://pulseai-hackshodh.pages.dev/"  # For development - restrict in production
+        "https://pulseai-hackshodh.pages.dev"  # For development - restrict in production
     ],
     allow_credentials=True,
     allow_methods=["*"],
